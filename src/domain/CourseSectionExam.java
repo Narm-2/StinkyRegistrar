2844fa6b3bf4ab0b630db91f134909b09f2e00ce
Sajjad <sssanikhani@gmail.com>
2021-06-26 16:16:28 +0430

    fix: change CSE class name to CourseSectionExam

--- src/domain/CourseSectionExam.java
+++ src/domain/CourseSectionExam.java
@@ -1,24 +1,24 @@
 package domain;
 import java.util.Date;
 
-public class CSE {
+public class CourseSectionExam {
 	private Course course;
 	private int section;
 	private Date examDate;
 
-	public CSE(Course course) {
+	public CourseSectionExam(Course course) {
 		this.course = course;
 		this.section = 1;
 		this.examDate = null;
 	}
 
-	public CSE(Course course, Date examDate) {
+	public CourseSectionExam(Course course, Date examDate) {
 		this.course = course;
 		this.section = 1;
 		this.examDate = examDate;
 	}
 
-	public CSE(Course course, Date examDate, int section) {
+	public CourseSectionExam(Course course, Date examDate, int section) {
 		this.course = course;
 		this.section = section;
 		this.examDate = examDate;
